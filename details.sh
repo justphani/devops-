f1e9f0ce7a048c5eb518f741a45da047b3a8b860
testuser1 <testuser1@ip-192-168-0-90.ec2.internal>
2021-10-20 10:14:51 +0000

    Added sudocode and users and list users in the machine

--- details.sh
+++ details.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 echo "This script will provide information about the local system"
+#GIVES HOSTNAME
+echo
 echo $(hostname)
+echo
+#GIVES USER EXECUTED THIS SCRIPT
+echo
 echo $(whoami)
-echo $(id -un)
+echo
+#GIVES USERS IN THE SYSTEM
+USERS=$(cat /etc/passwd | cut -d ":" -f 1)
+echo ${USERS}
